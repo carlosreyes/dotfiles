b6ff553a0a158421da0f6a146d8a34a137ed7283
Carlos Reyes <reyescarlos@live.com>
2015-02-16 07:29:07 -0500

    <*))))><

---
+++ config.fish
@@ -0,0 +1,17 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme budspencer
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins bundler z
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+set -U fish_key_bindings fish_vi_key_bindings
